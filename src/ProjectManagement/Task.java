40fbfc36ef73d652c6114ebc658212ff0b941583
Osman Vasquez <osman.vasquez@uabc.edu.mx>
2024-04-14 19:35:25 -0700

    Clases agregadas

--- src/ProjectManagement/Task.java
+++ src/ProjectManagement/Task.java
@@ -1,4 +1,37 @@
 package ProjectManagement;
 
 public class Task {
+    String name;
+    Employee[] theEmployees;
+    Resource[] theResources;
+
+    public Task(String name, Employee[] theEmployees, Resource[] theResources) {
+        this.name = name;
+        this.theEmployees = theEmployees;
+        this.theResources = theResources;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee[] getTheEmployees() {
+        return theEmployees;
+    }
+
+    public void setTheEmployees(Employee[] theEmployees) {
+        this.theEmployees = theEmployees;
+    }
+
+    public Resource[] getTheResources() {
+        return theResources;
+    }
+
+    public void setTheResources(Resource[] theResources) {
+        this.theResources = theResources;
+    }
 }
