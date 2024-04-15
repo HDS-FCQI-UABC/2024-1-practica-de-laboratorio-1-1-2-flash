c6fbb47c01fb701df88bfd92941e6e1345f8129c
JErnestoFN <142237844+JErnestoFN@users.noreply.github.com>
2024-04-14 19:48:08 -0700

    Add files via upload

---
+++ Resource.java
@@ -0,0 +1,16 @@
+package ProjectManagement;
+
+public class Resource {
+    private String name;
+    public Resource() {
+        this.name = "";
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
